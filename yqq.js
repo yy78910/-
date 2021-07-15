a3c1e1593eccea14686c1ab360f14d75930980e2
yy78910 <80239432+yy78910@users.noreply.github.com>
2021-07-15 15:03:44 +0800

    Add files via upload

---
+++ yqq.js
@@ -0,0 +1,10 @@
+		function ppp() {
+			alert('欢迎贾超贤老师');
+		}
+		function eee(rrr,bgColor) {
+			rrr.style.background=bgColor;
+		}
+		function zzz(rrr,bgColor) {
+			rrr.style.background=bgColor;
+		}
+		alert('欢迎贾老师');
